4b026be8b348ed5d5a001596aa1b727cec3d9d4d
kathyqat <k_qin@hotmail.com>
2021-12-06 15:55:01 -0500

    Render layout of gameboard

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-const playerFactory = (name) => {
+const playerFactory = (name,playerNum) => {
     const displayName = () => {
-        const mainDiv = document.querySelector('#main');
-        const nameDisplay = document.createElement('p');
-        nameDisplay.setAttribute('id', `${name}`);
+        const nameDisplay = document.querySelector(`#${playerNum}`);
         nameDisplay.textContent = name;
-        mainDiv.appendChild(nameDisplay);
     };
     return {displayName};
 };
   
 const gameBoard = (() => {
     'use strict';
+    const _displayNames = () => {
+        let username = prompt("Enter Player 1's name");
+        const player1 = playerFactory(username,'player1');
+        player1.displayName();
+        username = prompt("Enter Player 2's name");
+        const player2 = playerFactory(username,'player2');
+        player2.displayName();
+    };
+    
     const _clearNames = () => {
-        const mainDiv = document.querySelector('#main');
-        const names = document.querySelectorAll('p');
-        if (names){
-            names.forEach((p) => mainDiv.removeChild(p));
-        };
+        const player1 = playerFactory('','player1');
+        player1.displayName();
+        const player2 = playerFactory('','player2');
+        player2.displayName();
+    };
+    
+    const _drawGameBoard = () => {
+        const gameBoardDiv = document.querySelector('#gameBoard');
+        for (let i=0; i<9; i++){
+            const boardSection = document.createElement('div');
+            boardSection.setAttribute('id', `section${i}`);
+            boardSection.setAttribute('class', 'boardSection');
+            
+            const boardMarker = document.createElement('p');
+            boardMarker.setAttribute('id', `marker${i}`);
+            boardMarker.setAttribute('class', 'boardMarker');
+            
+            if (i%2==0){
+                boardMarker.textContent = 'X';
+                //const markerText = document.createTextNode('X');
+                //boardMarker.appendChild(markerText);
+            } else {
+                boardMarker.textContent = 'O';
+            };
+            boardSection.appendChild(boardMarker);
+            gameBoardDiv.appendChild(boardSection);
+        }
     };
     
     const init = () => {
+        _drawGameBoard();
         const startButton = document.querySelector('#startBtn');
         startButton.addEventListener('click', () => {
             _clearNames();
-            let username = prompt("Enter Player 1's name");
-            const player1 = playerFactory(username);
-            player1.displayName();
-
-            username = prompt("Enter Player 2's name");
-            const player2 = playerFactory(username);
-            player2.displayName();
+            _displayNames();
         });
     };
+    
     return {init};
-  })();
+})();
   
 gameBoard.init();
-
+  
 const displayController = (() => {
     'use strict';
 })();
